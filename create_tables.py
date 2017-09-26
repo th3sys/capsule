939bfe8e3344332c26655485e24e4b7184bc0bb1
Alexy Shelest <alexy.shelest@gmail.com>
2017-09-26 13:45:07 +0100

    add quotes table def

--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,43 @@
 from __future__ import print_function # Python 2/3 compatibility
 import boto3
 
+
+def create_quotes():
+    table = client.create_table(
+        TableName='Quotes.EOD',
+        KeySchema=[
+            {
+                'AttributeName': 'Symbol',
+                'KeyType': 'HASH'  # Partition key
+            },
+            {
+                'AttributeName': 'Date',
+                'KeyType': 'RANGE'  # Sort key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'Symbol',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'Date',
+                'AttributeType': 'S'
+            },
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+
+    w = client.get_waiter('table_exists')
+    w.wait(TableName='Quotes.EOD')
+    print("table Quotes.EOD created")
+    print("Table status:", table)
+
+
 def create_order():
     table = client.create_table(
         TableName='Orders',
@@ -36,6 +73,7 @@ def create_order():
     print("table Orders created")
     print("Table status:", table)
 
+
 def create_securities():
     table = client.create_table(
         TableName='Securities',
@@ -62,23 +100,29 @@ def create_securities():
     print("table Securities created")
     print("Table status:", table)
 
-#client = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://localhost:8000")
+
 client = boto3.client('dynamodb', region_name='us-east-1')
 
 try:
+    if 'Quotes.EOD' in client.list_tables()['TableNames']:
+        client.delete_table(TableName='Quotes.EOD')
+        waiter = client.get_waiter('table_not_exists')
+        waiter.wait(TableName='Quotes.EOD')
+        print("table Quotes.EOD deleted")
     if 'Orders' in client.list_tables()['TableNames']:
         client.delete_table(TableName='Orders')
         waiter = client.get_waiter('table_not_exists')
         waiter.wait(TableName='Orders')
-        print ("table Orders deleted")
+        print("table Orders deleted")
     if 'Securities' in client.list_tables()['TableNames']:
         client.delete_table(TableName='Securities')
         waiter = client.get_waiter('table_not_exists')
         waiter.wait(TableName='Securities')
-        print ("table Securities deleted")
+        print("table Securities deleted")
 
 except Exception as e:
     print(e)
 
 create_order()
 create_securities()
+create_quotes()
--- push_items.py
+++ push_items.py
@@ -9,7 +9,27 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 orders_table = dynamodb.Table('Orders')
 sec_table = dynamodb.Table('Securities')
+quotes_table = dynamodb.Table('Quotes.EOD')
 
+
+with open("quotes.json") as json_file:
+    quotes = json.load(json_file, parse_float = decimal.Decimal)
+    for quote in quotes:
+        Symbol = quote['Symbol']
+        Date = str(time.time())
+        Source = quote['Source']
+        Details = quote['Details']
+
+        print("Adding quote:", Symbol, Date)
+
+        quotes_table.put_item(
+           Item={
+               'Symbol': Symbol,
+               'Date': Date,
+               'Source': Source,
+               'Details': Details
+            }
+        )
 with open("securities.json") as json_file:
     securities = json.load(json_file, parse_float = decimal.Decimal)
     for security in securities:
