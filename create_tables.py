25388b4c2d7887bc591d9db3842787bcdfd6bc38
Alexy Shelest <alexy.shelest@gmail.com>
2017-11-10 12:49:41 +0000

    push security definition items

--- create_tables.py
+++ create_tables.py
@@ -38,42 +38,6 @@ def create_quotes():
     print("Table status:", table)
 
 
-def create_order():
-    table = client.create_table(
-        TableName='Orders',
-        KeySchema=[
-            {
-                'AttributeName': 'NewOrderId',
-                'KeyType': 'HASH'  # Partition key
-            },
-            {
-                'AttributeName': 'TransactionTime',
-                'KeyType': 'RANGE'  # Sort key
-            }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'NewOrderId',
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'TransactionTime',
-                'AttributeType': 'S'
-            },
-
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 10,
-            'WriteCapacityUnits': 10
-        }
-    )
-
-    w = client.get_waiter('table_exists')
-    w.wait(TableName='Orders')
-    print("table Orders created")
-    print("Table status:", table)
-
-
 def create_securities():
     table = client.create_table(
         TableName='Securities',
@@ -117,11 +81,6 @@ try:
         waiter = client.get_waiter('table_not_exists')
         waiter.wait(TableName='Quotes.EOD')
         print("table Quotes.EOD deleted")
-    if 'Orders' in client.list_tables()['TableNames']:
-        client.delete_table(TableName='Orders')
-        waiter = client.get_waiter('table_not_exists')
-        waiter.wait(TableName='Orders')
-        print("table Orders deleted")
     if 'Securities' in client.list_tables()['TableNames']:
         client.delete_table(TableName='Securities')
         waiter = client.get_waiter('table_not_exists')
@@ -131,6 +90,5 @@ try:
 except Exception as e:
     print(e)
 
-create_order()
 create_securities()
 create_quotes()
--- push_items.py
+++ push_items.py
@@ -7,10 +7,8 @@ import decimal
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
-orders_table = dynamodb.Table('Orders')
 sec_table = dynamodb.Table('Securities')
 quotes_table = dynamodb.Table('Quotes.EOD')
-
 
 with open("quotes.json") as json_file:
     quotes = json.load(json_file, parse_float = decimal.Decimal)
@@ -30,10 +28,12 @@ with open("quotes.json") as json_file:
                'Details': Details
             }
         )
+        
 with open("securities.json") as json_file:
     securities = json.load(json_file, parse_float = decimal.Decimal)
     for security in securities:
         Symbol = security['Symbol']
+        Broker = security['Broker']
         ProductType = security['ProductType']
         SubscriptionEnabled = bool(security['SubscriptionEnabled'])
         TradingEnabled = bool(security['TradingEnabled'])
@@ -45,6 +45,7 @@ with open("securities.json") as json_file:
         sec_table.put_item(
            Item={
                'Symbol': Symbol,
+               'Broker' : Broker,
                'ProductType': ProductType,
                'SubscriptionEnabled': SubscriptionEnabled,
                'TradingEnabled':TradingEnabled,
@@ -53,23 +54,3 @@ with open("securities.json") as json_file:
             }
         )
 
-with open("orders.json") as json_file:
-    orders = json.load(json_file, parse_float = decimal.Decimal)
-    for order in orders:
-        NewOrderId = str(uuid.uuid4().hex) #int(order['NewOrderId'])
-        TransactionTime = str(time.time()) #order['TransactionTime']
-        ClientOrderId = int(order['ClientOrderId'])
-        Status = order['Status']
-        Details = order['Details']
-
-        print("Adding order:", NewOrderId, TransactionTime)
-
-        orders_table.put_item(
-           Item={
-               'NewOrderId': NewOrderId,
-               'TransactionTime': TransactionTime,
-               'ClientOrderId': ClientOrderId,
-               'Status':Status,
-               'Details':Details
-            }
-        )
