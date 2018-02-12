44c28efa1500bf0ca484d05865aea193a9a95297
Alexy Shelest <alexy.shelest@gmail.com>
2018-02-12 08:56:04 +0000

    match pnl

--- pnl.py
+++ pnl.py
@@ -5,8 +5,10 @@ open_sell = False
 buy = 0.
 sell = 0.
 pnl = 0.
+
 with open('data/a_pnl.csv', 'w') as w:
     with open('data/results.csv') as f:
+        prev_day = 100
         for line in f:
             count += 1
             if count == 1:
@@ -24,16 +26,17 @@ with open('data/a_pnl.csv', 'w') as w:
                 open_buy = True
                 buy = future_close
                 line = line.rstrip('\n') + ',%s,,BUY\n' % str(-1*future_close)
-            elif days == 1 and open_sell:
+            elif (days == 1 or days > prev_day) and open_sell:
                 open_sell = False
-                line = line.rstrip('\n') + ',%s,%s,\n' % (str(-1*future_close), round(sell-future_close, 2))
+                line = line.rstrip('\n') + ',%s,%s,CS\n' % (str(-1*future_close), round(sell-future_close, 2))
                 print("Profit: %s" % (sell-future_close))
                 pnl += (sell-future_close)
-            elif days == 1 and open_buy:
+            elif (days == 1 or days > prev_day) and open_buy:
                 open_buy = False
-                line = line.rstrip('\n') + ',%s,%s,\n' % (str(future_close), round(future_close - buy, 2))
+                line = line.rstrip('\n') + ',%s,%s,CB\n' % (str(future_close), round(future_close - buy, 2))
                 print("Profit: %s" % (future_close - buy))
                 pnl += (future_close - buy)
+            prev_day = days
             w.write(line)
         print("PnL: %s" % pnl)
 
