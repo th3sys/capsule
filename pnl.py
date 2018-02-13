b957e1911440601e6476f7a80efdcc84fd9cf6e3
Alexy Shelest <alexy.shelest@gmail.com>
2018-02-13 13:08:02 +0000

    fix pnl

--- pnl.py
+++ pnl.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 
 count = 0
-open_buy = False
-open_sell = False
-buy = 0.
-sell = 0.
+position = 0
 pnl = 0.
+monthly = 0.
+annual = 0.
+prev_date = datetime.today()
 
 with open('data/a_pnl.csv', 'w') as w:
     with open('data/results.csv') as f:
@@ -15,28 +16,55 @@ with open('data/a_pnl.csv', 'w') as w:
                 w.write(line)
                 continue
             parts = line.split(',')
+            date = datetime.strptime(parts[4], '%Y%m%d')
             days = int(parts[8])
             future_close = float(parts[6])
             roll = float(parts[9])
-            if roll > 0.1 and not open_sell:
-                open_sell = True
-                sell = future_close
-                line = line.rstrip('\n') + ',%s,,SELL,\n' % str(future_close)
-            elif roll < -0.1 and not open_buy:
-                open_buy = True
-                buy = future_close
-                line = line.rstrip('\n') + ',%s,,BUY\n' % str(-1*future_close)
-            elif (days == 1 or days > prev_day) and open_sell:
-                open_sell = False
-                line = line.rstrip('\n') + ',%s,%s,CS\n' % (str(-1*future_close), round(sell-future_close, 2))
-                print("Profit: %s" % (sell-future_close))
-                pnl += (sell-future_close)
-            elif (days == 1 or days > prev_day) and open_buy:
-                open_buy = False
-                line = line.rstrip('\n') + ',%s,%s,CB\n' % (str(future_close), round(future_close - buy, 2))
-                print("Profit: %s" % (future_close - buy))
-                pnl += (future_close - buy)
+            if roll > 0.1 and position > -1 and days > 1:
+                position -= 1
+                pnl += future_close
+                monthly += future_close
+                annual += future_close
+                line = line.rstrip('\n') + ',%s,SELL,,\n' % (str(future_close))
+            elif roll < -0.1 and position < 1 and days > 1:
+                position += 1
+                pnl -= future_close
+                monthly -= future_close
+                annual -= future_close
+                line = line.rstrip('\n') + ',%s,BUY,\n' % (str(-1 * future_close))
+            elif (days == 1 or days > prev_day) and position < 0:
+                position = 0
+                pnl -= future_close
+                monthly -= future_close
+                annual -= future_close
+                print_anal = str(round(annual, 2)) if  date.month == 12 else ''
+                line = line.rstrip('\n') + ',{},CS,{},{},{}\n' \
+                    .format(str(-1 * future_close), round(monthly, 2), print_anal, round(pnl, 2))
+                print("Profit: %s" % monthly)
+                monthly = 0
+                if date.month == 12:
+                    annual = 0.
+            elif (days == 1 or days > prev_day) and position > 0:
+                position = 0
+                pnl += future_close
+                monthly += future_close
+                annual += future_close
+                print_anal = str(round(annual, 2)) if date.month == 12 else ''
+                line = line.rstrip('\n') + ',{},CB,{}, {}, {}\n' \
+                    .format(str(future_close), round(monthly, 2), print_anal, round(pnl, 2))
+                print("Profit: %s" % monthly)
+                monthly = 0
+                if date.month == 12:
+                    annual = 0.
+            elif days == 1:
+                print_anal = str(round(annual, 2)) if date.month == 12 else ''
+                line = line.rstrip('\n') + ',,CB,{}, {}, {}\n'.format(round(monthly, 2), print_anal,
+                                                                      round(pnl, 2))
+                print("Profit: %s" % monthly)
+                monthly = 0
+                if date.month == 12:
+                    annual = 0.
             prev_day = days
+            prev_date = date
             w.write(line)
         print("PnL: %s" % pnl)
-
